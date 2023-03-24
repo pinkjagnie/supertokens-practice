bacc751e80851eadad49219e1190bddb1acab0ca
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-24 09:58:32 +0100

    Implement dark theme

--- supertokens-app/frontend/src/config.tsx
+++ supertokens-app/frontend/src/config.tsx
@@ -22,16 +22,22 @@ export const SuperTokensConfig = {
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
     recipeList: [EmailPassword.init({
-        signInAndUpFeature: {
-            signInForm: {
-                formFields: [{
-                    id: "email",
-                    label: "customFieldName",
-                    placeholder: "Custom value"
-                }]
-            }
-        }
-    }), Session.init()],
+            style: `
+                [data-supertokens~=container] {
+                    --palette-background: 51, 51, 51;
+                    --palette-inputBackground: 41, 41, 41;
+                    --palette-inputBorder: 41, 41, 41;
+                    --palette-textTitle: 255, 255, 255;
+                    --palette-textLabel: 255, 255, 255;
+                    --palette-textPrimary: 255, 255, 255;
+                    --palette-error: 173, 46, 46;
+                    --palette-textInput: 169, 169, 169;
+                    --palette-textLink: 169, 169, 169;
+                    --palette-superTokensBrandingBackground: 173, 189, 196;
+                    --palette-superTokensBrandingText: 255, 255, 255;
+                }
+            `
+        }), Session.init()],
 };
 
 export const recipeDetails = {
